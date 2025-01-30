8e640b23368b85831490df990d04b6e5a4bb70b8
Priscilla Oliveira <130604610+pryscw@users.noreply.github.com>
2025-01-30 17:07:00 -0300

    Armazenando valores de um arquivo em um dicionário

---
+++ Files/file_dict/file_dict/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace file_dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> Usuarios = new Dictionary<string, string>();
+
+            var path = "usuarios.txt";
+            using (StreamReader sr = new StreamReader(File.OpenRead(path)))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string linha = sr.ReadLine();
+                    string[] valores = linha.Split(" ");
+                    string id = valores[0];
+                    string name = valores[1];
+
+                    Usuarios.Add(id, name);
+                }
+            }
+
+            foreach (KeyValuePair<string, string> valuePair in Usuarios)
+            {
+                Console.WriteLine($"{valuePair.Key} - {valuePair.Value}");
+            }
+
+        }
+    }
+}
